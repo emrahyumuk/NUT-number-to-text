35e5040fb5e98920fb27c6ca8d2092e13aefd0cb
Emrah Y. <emrahyumuk@gmail.com>
2014-04-02 09:04:21 +0300

    decimal 0 bug fixed for money converter

--- src/Nut/TextConverters/CurrencyPartials/BaseConverter.cs
+++ src/Nut/TextConverters/CurrencyPartials/BaseConverter.cs
@@ -7,26 +7,31 @@ using System.Threading.Tasks;
 using Nut.Models;
 
 namespace Nut.TextConverters {
-     internal abstract partial class BaseConverter {
+    internal abstract partial class BaseConverter {
 
-         public virtual string ToTextWithCurrency(decimal num, Currency currency) {
+        public virtual string ToTextWithCurrency(decimal num, Currency currency) {
 
             var builder = new StringBuilder();
             var currencyModel = GetCurrencyModel(currency);
-             var decimalSeperator = num.ToString().Contains(",") ? ',' : '.';
-             var nums = num.ToString().Split(decimalSeperator);
+            var decimalSeperator = num.ToString().Contains(",") ? ',' : '.';
+            var nums = num.ToString().Split(decimalSeperator);
 
             var mainNum = Convert.ToInt64(nums[0]);
             builder.Append(ToText(mainNum));
             builder.Append(" ");
             builder.Append(GetCurrencyText(mainNum, currencyModel));
 
-            if (nums.Count() > 1 && !string.IsNullOrEmpty(nums[1])) {
+            if (nums.Count() > 1 && !string.IsNullOrEmpty(nums[1])) 
+            {
                 var childNum = Convert.ToInt64(nums[1].Substring(0, 2));
-                builder.Append(" ");
-                builder.Append(ToText(childNum));
-                builder.Append(" ");
-                builder.Append(GetChildCurrencyText(childNum, currencyModel));
+                if (childNum != 0) 
+                {
+                    builder.Append(" ");
+                    builder.Append(ToText(childNum));
+                    builder.Append(" ");
+                    builder.Append(GetChildCurrencyText(childNum, currencyModel));
+                }
+
             }
 
             return builder.ToString().Trim();
