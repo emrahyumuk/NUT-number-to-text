42b9acb2bb908544204d79c84f3c8d0c501cd786
Stepami <Stepami@mail.ru>
2026-04-17 19:12:18 +0300

    chore: #25 - compiler warnings

--- src/Nut.Tests/ToTextTestBase.cs
+++ src/Nut.Tests/ToTextTestBase.cs
@@ -6,7 +6,7 @@
         {
             var moneyText = number.ToText(currency, lang).ToLower();
 
-            Assert.AreEqual(expectedText, moneyText);
+            Assert.That(moneyText, Is.EqualTo(expectedText));
         }
     }
 }
