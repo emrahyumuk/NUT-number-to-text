654f5ef872f383d5fdf973f75f3d58db7f5d2395
Stepami <Stepami@mail.ru>
2026-04-17 19:14:33 +0300

    test: #25 - test cases

--- src/Nut.Tests/Thousands.cs
+++ src/Nut.Tests/Thousands.cs
@@ -5,6 +5,8 @@
         [TestCase("pln", "pl-PL", 1_000, "jeden tysiąc złotych zero groszy")]
         [TestCase("pln", "pl-PL", 3_000, "trzy tysiące złotych zero groszy")]
         [TestCase("pln", "pl-PL", 5_000, "pięć tysięcy złotych zero groszy")]
+        [TestCase("rub", "ru-RU", 41_000, "сорок одна тысяча рублей ноль копеек")]
+        [TestCase("rub", "ru-RU", 42_000, "сорок две тысяча рублей ноль копеек")]
         public void ToText_ForDecimalNumber_ExpectCorrectText(string currency, string lang, decimal number, string expectedText)
         {
             base.BaseToText_ForDecimalNumber_ExpectCorrectText(currency, lang, number, expectedText);
